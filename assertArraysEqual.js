fad32102b51066f82e19e7cfa19d81b2f1f051bd
Rico Chow <rico.ck.chow@gmail.com>
2023-09-22 12:18:43 -0400

    Add assertArrays Equal and fic eqArrays function.

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+let eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1,2,4], [1,2,3]));
+
+
+let assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log('The arrays are not the same.');
+  } else if (eqArrays(arr1,arr2) === true) {
+    console.log('The arrays are identical!');
+  }
+};
+assertArraysEqual([1,3,3],[1,2,3]);
+assertArraysEqual([1,2,3],[1,2,3]);
--- eqArrays.js
+++ eqArrays.js
@@ -22,10 +22,9 @@ let eqArrays = function(arr1, arr2) {
   for (let i = 0; i <= arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-    } else {
-      return true;
     }
   }
+  return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
