37c8e8c42f478ac28180f5515b537448ef0dbdce
Rico Chow <rico.ck.chow@gmail.com>
2023-09-24 09:55:46 -0400

    Add without.js.

---
+++ without.js
@@ -0,0 +1,22 @@
+let eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log('The arrays are not the same.');
+  } else if (eqArrays(arr1,arr2) === true) {
+    console.log('The arrays are identical!');
+  }
+};
+
+let without = function(source, itemsToRemove) {
+  let newArr = source.filter(item => !itemsToRemove.includes(item));
+  return newArr;
+};
+
