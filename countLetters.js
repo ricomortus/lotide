5a2e965b55adbcc6be52687638e1c8da2b6f00b6
Rico Chow <rico.ck.chow@gmail.com>
2023-09-25 10:29:05 -0400

    Add countLetters.js.

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅lsAssertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let results = {};
+  for (const letter of str) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+const result1 = countLetters('Hello');
+
+assertEqual(result1['H'], 1);
+assertEqual(result1['e'], 1);
+assertEqual(result1['l'], 2);
+assertEqual(result1['o'], 1);
