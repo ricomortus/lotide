1e44a40cdb548160a7c1cefae0f46dd44902b4b7
Rico Chow <rico.ck.chow@gmail.com>
2023-09-27 13:19:19 -0400

    Add takeUntil.js with updated eqArrays function.

---
+++ takeUntil.js
@@ -0,0 +1,49 @@
+//assertArraysEquals Function
+let eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    console.log('The arrays are not the same.');
+  } else if (eqArrays(arr1,arr2) === true) {
+    console.log('The arrays are identical!');
+  }
+};
+
+
+//input: array, callback
+//output: return "slice of the array with elements taken from the beginning".
+
+//Callback input: one value (item in array)
+
+//Higher Order Function
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    if (callback(item) === false) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+};
+
+//Testing
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood']);
