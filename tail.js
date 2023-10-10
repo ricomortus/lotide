763420313ccd61f039ff451930709aa5873d7252
Rico Chow <rico.ck.chow@gmail.com>
2023-10-10 09:50:30 -0400

    Refactor tail into test file.

--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
-//FUNCTION IMPLEMENTATION
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅lsAssertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   const newArr = arr.slice(1, arr.length);
   console.log(newArr);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
+
